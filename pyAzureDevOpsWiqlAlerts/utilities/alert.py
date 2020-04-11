08988e5d730a4003717372bb781ba851baac5d2d
akloc@github.com <akloc@github.com>
2020-04-11 14:11:48 -0400

    Switched to PYCharm

--- pyAzureDevOpsWiqlAlerts/utilities/alert.py
+++ pyAzureDevOpsWiqlAlerts/utilities/alert.py
@@ -1,10 +1,15 @@
 #! python3
+from pyAzureDevOpsWiqlAlerts.utilities.azuredevopsinterface import AzureDevopsInterface
+from pyAzureDevOpsWiqlAlerts.utilities.slackinterface import SlackInterface
 
-class alert:
 
-    def __init__(self, alert_name):
-        print('constructing.')
+class Alert:
+
+    def __init__(self, alert_name, slack_webhook_uri):
+        print('Building alert.')
         self._alert_name = alert_name
+        self._slack_webhook_uri = slack_webhook_uri
+        self._slack_interface = SlackInterface(self._slack_webhook_uri)
 
     @property
     def alert_name(self):
@@ -21,12 +26,16 @@ class alert:
     @azure_devops_query_config.setter
     def azure_devops_query_config(self, value):
         if 'api_token' in value and 'query_id' in value \
-            and 'query_id' in value:
+                and 'query_uri' in value:
             self._azure_devops_query_config = value
+            self._azure_devops_interface = AzureDevopsInterface(
+                uri=self._azure_devops_query_config['query_uri'],
+                personal_access_token=self._azure_devops_query_config['api_token']
+            )
         else:
             raise ValueError('api_token, query_id, and query_uri \
                 must be defined in the dictionary.')
-        
+
     @property
     def slack_webhook_uri(self):
         return self._slack_webhook_uri
@@ -34,3 +43,7 @@ class alert:
     @slack_webhook_uri.setter
     def slack_webhook_uri(self, value):
         self._slack_webhook_uri = value
+        self._slack_interface = SlackInterface(self._slack_webhook_uri)
+
+    def process(self, message):
+        self._slack_interface.send_message(message=message, channel=None)
