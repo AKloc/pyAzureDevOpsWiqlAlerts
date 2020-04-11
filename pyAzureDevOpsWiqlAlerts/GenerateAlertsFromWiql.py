967b1a68eba25b7f3388455c96336398d9dc7d22
akloc@github.com <akloc@github.com>
2020-04-11 15:41:13 -0400

    Cleanup.

--- pyAzureDevOpsWiqlAlerts/GenerateAlertsFromWiql.py
+++ pyAzureDevOpsWiqlAlerts/GenerateAlertsFromWiql.py
@@ -3,6 +3,7 @@ import json
 import os
 from pyAzureDevOpsWiqlAlerts.utilities import azuredevopsinterface
 
+
 def lambda_handler(event, context):
     # Fetch AWS environment variables
     if 'AZURE_DEVOPS_URI' in os.environ.keys():
@@ -14,16 +15,17 @@ def lambda_handler(event, context):
         azure_devops_access_token = os.environ['AZURE_DEVOPS_ACCESS_TOKEN']
     else:
         azure_devops_access_token = 'no access token set'
-    
+
     f = azuredevopsinterface(azure_devops_uri, azure_devops_access_token)
     f.passthrough_method('Derp')
 
-    f.get_azure_devops_work_items_from_query_id(\
+    f.get_azure_devops_work_items_from_query_id( \
         query_id='705fc4ed-11f7-41f1-bdf0-e3f0197a4598')
 
     return {
         'statusCode': 200,
         'body': json.dumps('azure_devops_interface did stuff.')
     }
+
 
 lambda_handler(None, None)
