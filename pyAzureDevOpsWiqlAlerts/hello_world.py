754be25758e7ee62d2ddf37983f9de39b217d8ac
akloc@github.com <akloc@github.com>
2020-04-11 15:41:13 -0400

    Cleanup.

--- pyAzureDevOpsWiqlAlerts/hello_world.py
+++
@@ -1,9 +0,0 @@
-#!python3
-import json
-
-def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda - MODIFIED!')
-    }
