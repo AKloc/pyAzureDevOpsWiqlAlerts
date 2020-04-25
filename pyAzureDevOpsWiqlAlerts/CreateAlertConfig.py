c087767373f9cdbd7d6df03a15df97d5931adc21
akloc@github.com <akloc@github.com>
2020-04-24 22:18:31 -0400

    Added code to insert data, needs to be wired up to API Gateway.

--- pyAzureDevOpsWiqlAlerts/CreateAlertConfig.py
+++ pyAzureDevOpsWiqlAlerts/CreateAlertConfig.py
@@ -1,11 +1,15 @@
 #! python3
 import json
+from pyAzureDevOpsWiqlAlerts.utilities.AlertCreator import AlertCreator
 
 def lambda_handler(event, context):
+
+    alert_creator = AlertCreator()
 
     return {
         'statusCode': 201,
         'body': json.dumps('Alert successfully created.')
     }
+
 
 lambda_handler(None, None)
