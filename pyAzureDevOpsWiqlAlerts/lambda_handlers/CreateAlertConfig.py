67540d3fdc55677ba8f6ac1a6761788f24edc313
akloc@github.com <akloc@github.com>
2020-04-28 00:16:57 -0400

    Added a sys.path.append call to the lambda handlers - AWS needs that to get the opt directory that the layer unzips to onto the system path. Alternative solution: when zipping the code for the layer, wrap it in a directory called "Python", which is included in the PATH already.

--- pyAzureDevOpsWiqlAlerts/lambda_handlers/CreateAlertConfig.py
+++ pyAzureDevOpsWiqlAlerts/lambda_handlers/CreateAlertConfig.py
@@ -1,5 +1,7 @@
-#! python3
+#!python3
 import json
+import sys
+sys.path.append("/opt/")
 from pyAzureDevOpsWiqlAlerts.utilities.AlertCreator import AlertCreator
 
 def lambda_handler(event, context):
